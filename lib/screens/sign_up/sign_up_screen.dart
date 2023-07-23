7d5de7baabfc06d32193742d20cc0edf82392d55
이태언 <mktears0609@gmail.com>
2023-07-23 21:01:09 +0900

    ADD: sign up text align

--- lib/screens/sign_up/sign_up_screen.dart
+++ lib/screens/sign_up/sign_up_screen.dart
@@ -9,20 +9,31 @@ class SignUpScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: const [
-            Gaps.v80,
-            Text(
-              "Sign up for TikTok",
-              style: TextStyle(
-                fontSize: Sizes.size24,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size40,
+          ),
+          child: Column(
+            children: const [
+              Gaps.v80,
+              Text(
+                "Sign up for TikTok",
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
-            ),
-            Gaps.v20,
-            Text(
-              "Create a profile, follow other accounts, make your own videos, and more.",
-            ),
-          ],
+              Gaps.v20,
+              Text(
+                "Create a profile, follow other accounts, make your own videos, and more.",
+                style: TextStyle(
+                  fontSize: Sizes.size16,
+                  color: Colors.black45,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
