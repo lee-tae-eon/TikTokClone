e4308271dcd6a9d090f4822bbe3bb3db3143c69f
이태언 <mktears0609@gmail.com>
2023-07-23 20:57:48 +0900

    ADD :font

--- lib/screens/sign_up/sign_up_screen.dart
+++ lib/screens/sign_up/sign_up_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ticktok_clone/constants/gaps.dart';
+import 'package:ticktok_clone/constants/sizes.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
@@ -6,12 +8,26 @@ class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: const [
-        Text(
-          "Sign up for TikTok",
+      body: SafeArea(
+        child: Column(
+          children: const [
+            Gaps.v80,
+            Text(
+              "Sign up for TikTok",
+              style: TextStyle(
+                fontSize: Sizes.size24,
+              ),
+            ),
+            Gaps.v20,
+            Text(
+              "Create a profile, follow other accounts, make your own videos, and more.",
+            ),
+          ],
         ),
-      ],
-    ));
+      ),
+    );
   }
 }
+
+
+// * safeArea (상단 status bar 같은곳은 가려지지 않도록 해주는 위젯)
