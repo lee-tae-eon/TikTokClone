d365cb55e23e4f3cfede154f04cef26e00bd1800
이태언 <mktears0609@gmail.com>
2023-07-27 20:09:16 +0900

    ADD: FractionallySizedBox

--- lib/features/authentication/common/widgets/auth_button.dart
+++ lib/features/authentication/common/widgets/auth_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticktok_clone/constants/sizes.dart';
 
 class AuthButton extends StatelessWidget {
   final String text;
@@ -7,8 +8,17 @@ class AuthButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(text),
+    // * FractionallySizedBox :  box 위젯 픽셀 정렬이 아닌 부모의 크기에 비례해서 크기를 정해주는 위젯
+    return FractionallySizedBox(
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.grey.shade200,
+            width: Sizes.size2,
+          ),
+        ),
+        child: Text(text),
+      ),
     );
   }
 }
