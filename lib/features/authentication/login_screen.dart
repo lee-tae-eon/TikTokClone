83a4f009df203a48b13a881ea76d28399b65d00f
이태언 <mktears0609@gmail.com>
2023-08-05 17:59:24 +0900

    FIX: folder 변경

--- lib/features/authentication/login_screen.dart
+++ lib/features/authentication/login_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ticktok_clone/constants/gaps.dart';
 import 'package:ticktok_clone/constants/sizes.dart';
-import 'package:ticktok_clone/features/authentication/common/widgets/auth_button.dart';
+import 'package:ticktok_clone/features/authentication/widgets/auth_button.dart';
 import 'package:ticktok_clone/features/authentication/sign_up_screen.dart';
 
 class LoginScreen extends StatelessWidget {
