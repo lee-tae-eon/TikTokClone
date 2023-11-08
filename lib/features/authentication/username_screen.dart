6d6abe064c0db150efd779e2ee3fa74e650a8f1b
이태언 <mktears0609@gmail.com>
2023-11-08 19:24:17 +0900

    ADD: context

--- lib/features/authentication/username_screen.dart
+++ lib/features/authentication/username_screen.dart
@@ -34,6 +34,8 @@ class _UsernameScreenState extends State<UsernameScreen> {
     });
   }
 
+//* StatefulWidget 의 state안에 있으면 BuilderContext를 받을 필요가 없다.
+// * 어디서든 context를 사용할 수 있다.
   void onNextTap() {
     if (_usernameController.text.isEmpty) return;
     Navigator.of(context).push(
