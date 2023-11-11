136b26fb4bfcf756a88ed06ce6adce24b1612dac
이태언 <mktears0609@gmail.com>
2023-11-11 12:08:52 +0900

    FIX: email to password

--- lib/features/authentication/password_screen.dart
+++ lib/features/authentication/password_screen.dart
@@ -1,10 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:ticktok_clone/constants/gaps.dart';
+import 'package:ticktok_clone/constants/sizes.dart';
+import 'package:ticktok_clone/features/authentication/widgets/form_button.dart';
 
-class PasswordScreen extends StatelessWidget {
+class PasswordScreen extends StatefulWidget {
   const PasswordScreen({super.key});
 
   @override
+  State<PasswordScreen> createState() => _PasswordScreenState();
+}
+
+class _PasswordScreenState extends State<PasswordScreen> {
+  // * 이벤트 리스너를 추가하기 때문에 dispose를 해야한다.
+  // * dispose 를 하지 않으면 memory 부족으로 crash 날 확률이 잇다.
+  final TextEditingController _passwordController = TextEditingController();
+  String _email = "";
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    // * 모든 이벤트를 dispose하고  super 를 dispose한다
+    super.dispose();
+  }
+
+  String? _isEmailValid() {
+    if (_email.isEmpty) return null;
+    final regExp = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    if (!regExp.hasMatch(_email)) {
+      return "Email not valid";
+    }
+    return null;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordController.addListener(() {
+      setState(() {
+        _email = _passwordController.text;
+      });
+    });
+  }
+
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
+  void _onSubmit() {
+    if (_email.isEmpty || _isEmailValid() != null) return;
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const PasswordScreen(),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Sign up",
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size36,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v40,
+              const Text(
+                "What is your email?",
+                style: TextStyle(
+                  fontSize: Sizes.size20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Gaps.v16,
+              TextField(
+                controller: _passwordController,
+                // * keyboard를 email 타입으로
+                keyboardType: TextInputType.emailAddress,
+                // * onComplete와 onEditingComplete가 있는데
+                // * onComplete는 context(text) 를 인자로 받는다 이미 알고 있기 때문에
+                // * onEditingComplete를 쓴다.
+                onEditingComplete: _onSubmit,
+                autocorrect: false,
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  errorText: _isEmailValid(),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                cursorColor: Theme.of(context).primaryColor,
+              ),
+              Gaps.v16,
+              GestureDetector(
+                  onTap: _onSubmit,
+                  child: FormButton(
+                      disabled: _email.isEmpty || _isEmailValid() != null)),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
