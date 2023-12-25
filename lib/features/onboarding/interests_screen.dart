2c9e5fe4bcdc7d7bbdbc01b91a9ef65e8c3090ad
이태언 <mktears0609@gmail.com>
2023-12-25 20:07:13 +0900

    ADD: list

--- lib/features/onboarding/interests_screen.dart
+++ lib/features/onboarding/interests_screen.dart
@@ -2,6 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:ticktok_clone/constants/gaps.dart';
 import 'package:ticktok_clone/constants/sizes.dart';
 
+const interests = [
+  "Daily Life",
+  "Comedy",
+  "Entertainment",
+  "Animals",
+  "Food",
+  "Beauty & Style",
+  "Drama",
+  "Learning",
+  "Talent",
+  "Sports",
+  "Auto",
+  "Family",
+  "Fitness & Health",
+  "DIY & Life Hacks",
+  "Arts & Crafts",
+  "Dance",
+  "Outdoors",
+  "Oddly Satisfying",
+  "Home & Garden",
+  "Daily Life",
+  "Comedy",
+  "Entertainment",
+  "Animals",
+  "Food",
+  "Beauty & Style",
+  "Drama",
+  "Learning",
+  "Talent",
+  "Sports",
+  "Auto",
+  "Family",
+  "Fitness & Health",
+  "DIY & Life Hacks",
+  "Arts & Crafts",
+  "Dance",
+  "Outdoors",
+  "Oddly Satisfying",
+  "Home & Garden",
+];
+
 class InterestsScreen extends StatelessWidget {
   const InterestsScreen({super.key});
 
@@ -18,17 +59,22 @@ class InterestsScreen extends StatelessWidget {
           horizontal: Sizes.size24,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
             Gaps.v32,
             Text(
               "Choose your interests",
               style: TextStyle(
                 fontSize: Sizes.size40,
+                fontWeight: FontWeight.bold,
               ),
             ),
             Gaps.v20,
             Text(
               "Get better video recommendations",
+              style: TextStyle(
+                fontSize: Sizes.size20,
+              ),
             ),
           ],
         ),
