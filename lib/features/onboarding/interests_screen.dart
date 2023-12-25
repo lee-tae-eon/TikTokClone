940301ef36f6a21fdc4c4a3f09be57b0174f8cef
이태언 <mktears0609@gmail.com>
2023-12-26 06:42:41 +0900

    ADD: overflow scroll view + color

--- lib/features/onboarding/interests_screen.dart
+++ lib/features/onboarding/interests_screen.dart
@@ -54,59 +54,68 @@ class InterestsScreen extends StatelessWidget {
           "Choose your interests",
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: Sizes.size24,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Gaps.v32,
-            const Text(
-              "Choose your interests",
-              style: TextStyle(
-                fontSize: Sizes.size40,
-                fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size24,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v32,
+              const Text(
+                "Choose your interests",
+                style: TextStyle(
+                  fontSize: Sizes.size40,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Gaps.v20,
-            const Text(
-              "Get better video recommendations",
-              style: TextStyle(
-                fontSize: Sizes.size20,
+              Gaps.v20,
+              const Text(
+                "Get better video recommendations",
+                style: TextStyle(
+                  fontSize: Sizes.size20,
+                ),
               ),
-            ),
-            Gaps.v64,
-            // * Wrap widget 은 children을 가로로 배치 한줄에 다 들어가지 않으면 flex-wrap처럼 작동한다.
-            Wrap(
-              children: [
-                for (var interest in interests)
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: Sizes.size12,
-                      horizontal: Sizes.size12,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(
-                        Sizes.size32,
+              Gaps.v64,
+              // * Wrap widget 은 children을 가로로 배치 한줄에 다 들어가지 않으면 flex-wrap처럼 작동한다.
+              Wrap(
+                runSpacing: 20,
+                spacing: 20,
+                children: [
+                  for (var interest in interests)
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: Sizes.size12,
+                        horizontal: Sizes.size12,
                       ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.05),
-                          blurRadius: 5,
-                          spreadRadius: 5,
-                        )
-                      ],
-                    ),
-                    child: Text(interest,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                        )),
-                  )
-              ],
-            )
-          ],
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(
+                          0.05,
+                        ),
+                        border: Border.all(
+                          color: Colors.black38,
+                        ),
+                        borderRadius: BorderRadius.circular(
+                          Sizes.size32,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.05),
+                            blurRadius: 5,
+                            spreadRadius: 5,
+                          )
+                        ],
+                      ),
+                      child: Text(interest,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          )),
+                    )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
