537f50d4f57426020bc77d09d91f58bd386a190f
이태언 <mktears0609@gmail.com>
2023-12-25 20:04:23 +0900

    ADD: text style

--- lib/features/onboarding/interests_screen.dart
+++ lib/features/onboarding/interests_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ticktok_clone/constants/gaps.dart';
+import 'package:ticktok_clone/constants/sizes.dart';
 
 class InterestsScreen extends StatelessWidget {
   const InterestsScreen({super.key});
@@ -9,6 +11,26 @@ class InterestsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text(
           "Choose your interests",
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size24,
+        ),
+        child: Column(
+          children: const [
+            Gaps.v32,
+            Text(
+              "Choose your interests",
+              style: TextStyle(
+                fontSize: Sizes.size40,
+              ),
+            ),
+            Gaps.v20,
+            Text(
+              "Get better video recommendations",
+            ),
+          ],
         ),
       ),
     );
