5a7071a75eacee34a617d05d8e4c3b1ff23ea7c9
이태언 <mktears0609@gmail.com>
2023-12-30 20:45:21 +0900

    ADD: init & dispose

--- lib/features/onboarding/interests_screen.dart
+++ lib/features/onboarding/interests_screen.dart
@@ -54,6 +54,16 @@ class _InterestsScreenState extends State<InterestsScreen> {
   final ScrollController _scrollController = ScrollController();
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
