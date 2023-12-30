f1eb8d0e1b759864b8340b1bdcbe752479ba469d
이태언 <mktears0609@gmail.com>
2023-12-30 20:34:01 +0900

    ADD: scroll controller

--- lib/features/onboarding/interests_screen.dart
+++ lib/features/onboarding/interests_screen.dart
@@ -43,8 +43,15 @@ const interests = [
   "Home & Garden",
 ];
 
-class InterestsScreen extends StatelessWidget {
+class InterestsScreen extends StatefulWidget {
   const InterestsScreen({super.key});
+
+  @override
+  State<InterestsScreen> createState() => _InterestsScreenState();
+}
+
+class _InterestsScreenState extends State<InterestsScreen> {
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -54,67 +61,70 @@ class InterestsScreen extends StatelessWidget {
           "Choose your interests",
         ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            left: Sizes.size24,
-            right: Sizes.size24,
-            bottom: Sizes.size16,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Gaps.v32,
-              const Text(
-                "Choose your interests",
-                style: TextStyle(
-                  fontSize: Sizes.size40,
-                  fontWeight: FontWeight.bold,
+      body: Scrollbar(
+        controller: _scrollController,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: Sizes.size24,
+              right: Sizes.size24,
+              bottom: Sizes.size16,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Gaps.v32,
+                const Text(
+                  "Choose your interests",
+                  style: TextStyle(
+                    fontSize: Sizes.size40,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Gaps.v20,
-              const Text(
-                "Get better video recommendations",
-                style: TextStyle(
-                  fontSize: Sizes.size20,
+                Gaps.v20,
+                const Text(
+                  "Get better video recommendations",
+                  style: TextStyle(
+                    fontSize: Sizes.size20,
+                  ),
                 ),
-              ),
-              Gaps.v64,
-              // * Wrap widget 은 children을 가로로 배치 한줄에 다 들어가지 않으면 flex-wrap처럼 작동한다.
-              Wrap(
-                runSpacing: 15,
-                spacing: 15,
-                children: [
-                  // * list 가 많은 경우 ListView Builder를 사용하자
-                  for (var interest in interests)
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: Sizes.size12,
-                        horizontal: Sizes.size12,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        border:
-                            Border.all(color: Colors.black.withOpacity(0.1)),
-                        borderRadius: BorderRadius.circular(
-                          Sizes.size32,
+                Gaps.v64,
+                // * Wrap widget 은 children을 가로로 배치 한줄에 다 들어가지 않으면 flex-wrap처럼 작동한다.
+                Wrap(
+                  runSpacing: 15,
+                  spacing: 15,
+                  children: [
+                    // * list 가 많은 경우 ListView Builder를 사용하자
+                    for (var interest in interests)
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: Sizes.size12,
+                          horizontal: Sizes.size12,
                         ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.05),
-                            blurRadius: 5,
-                            spreadRadius: 5,
-                          )
-                        ],
-                      ),
-                      child: Text(interest,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                          )),
-                    )
-                ],
-              )
-            ],
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border:
+                              Border.all(color: Colors.black.withOpacity(0.1)),
+                          borderRadius: BorderRadius.circular(
+                            Sizes.size32,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.05),
+                              blurRadius: 5,
+                              spreadRadius: 5,
+                            )
+                          ],
+                        ),
+                        child: Text(interest,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            )),
+                      )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
