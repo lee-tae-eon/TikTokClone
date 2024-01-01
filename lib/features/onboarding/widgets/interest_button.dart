c850311c61491a1478c48d88af910345862fc4f6
이태언 <mktears0609@gmail.com>
2024-01-01 10:15:05 +0900

    ADD: statefull

--- lib/features/onboarding/widgets/interest_button.dart
+++ lib/features/onboarding/widgets/interest_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ticktok_clone/constants/sizes.dart';
 
-class InterestButton extends StatelessWidget {
+class InterestButton extends StatefulWidget {
   const InterestButton({
     super.key,
     required this.interest,
@@ -10,30 +10,46 @@ class InterestButton extends StatelessWidget {
   final String interest;
 
   @override
+  State<InterestButton> createState() => _InterestButtonState();
+}
+
+class _InterestButtonState extends State<InterestButton> {
+  bool _isSelected = false;
+
+  void _onTap() {
+    setState(() {
+      _isSelected = !_isSelected;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        vertical: Sizes.size12,
-        horizontal: Sizes.size12,
-      ),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(color: Colors.black.withOpacity(0.1)),
-        borderRadius: BorderRadius.circular(
-          Sizes.size32,
+    return GestureDetector(
+      onTap: _onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: Sizes.size12,
+          horizontal: Sizes.size12,
         ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.05),
-            blurRadius: 5,
-            spreadRadius: 5,
-          )
-        ],
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.black.withOpacity(0.1)),
+          borderRadius: BorderRadius.circular(
+            Sizes.size32,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.05),
+              blurRadius: 5,
+              spreadRadius: 5,
+            )
+          ],
+        ),
+        child: Text(widget.interest,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            )),
       ),
-      child: Text(interest,
-          style: const TextStyle(
-            fontWeight: FontWeight.bold,
-          )),
     );
   }
 }
