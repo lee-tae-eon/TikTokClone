1fb9650856ab190cddd1232c0a7730cca49a0135
이태언 <mktears0609@gmail.com>
2024-01-14 20:38:00 +0900

    ADD: tab bar view

--- lib/features/onboarding/tutorial_screen.dart
+++ lib/features/onboarding/tutorial_screen.dart
@@ -15,87 +15,88 @@ class _TutorialScreenState extends State<TutorialScreen> {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-          body: SafeArea(
-        child: TabBarView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: Sizes.size24,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Gaps.v52,
-                  Text(
-                    "Watch cool videos!",
-                    style: TextStyle(
-                      fontSize: Sizes.size40,
-                      fontWeight: FontWeight.bold,
+        body: SafeArea(
+          child: TabBarView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Sizes.size24,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Gaps.v52,
+                    Text(
+                      "Watch cool videos!",
+                      style: TextStyle(
+                        fontSize: Sizes.size40,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  Gaps.v16,
-                  Text(
-                    "Videos are personalized for you based on what you watch, like, and share.",
-                    style: TextStyle(
-                      fontSize: Sizes.size20,
+                    Gaps.v16,
+                    Text(
+                      "Videos are personalized for you based on what you watch, like, and share.",
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: Sizes.size24,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Gaps.v52,
-                  Text(
-                    "Watch cool videos!",
-                    style: TextStyle(
-                      fontSize: Sizes.size40,
-                      fontWeight: FontWeight.bold,
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Sizes.size24,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Gaps.v52,
+                    Text(
+                      "Follow the rules!",
+                      style: TextStyle(
+                        fontSize: Sizes.size40,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  Gaps.v16,
-                  Text(
-                    "Videos are personalized for you based on what you watch, like, and share.",
-                    style: TextStyle(
-                      fontSize: Sizes.size20,
+                    Gaps.v16,
+                    Text(
+                      "Videos are personalized for you based on what you watch, like, and share.",
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: Sizes.size24,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Gaps.v52,
-                  Text(
-                    "Watch cool videos!",
-                    style: TextStyle(
-                      fontSize: Sizes.size40,
-                      fontWeight: FontWeight.bold,
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Sizes.size24,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Gaps.v52,
+                    Text(
+                      "Enjoy the ride",
+                      style: TextStyle(
+                        fontSize: Sizes.size40,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  Gaps.v16,
-                  Text(
-                    "Videos are personalized for you based on what you watch, like, and share.",
-                    style: TextStyle(
-                      fontSize: Sizes.size20,
+                    Gaps.v16,
+                    Text(
+                      "Videos are personalized for you based on what you watch, like, and share.",
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            )
-          ],
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
