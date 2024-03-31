fd42bc25f3547f69fe0badd35265345e21e0f41e
이태언 <mktears0609@gmail.com>
2024-04-01 05:59:36 +0900

    ADD: bottomnavigation bar

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainNavigationScreen extends StatelessWidget {
-  const MainNavigationScreen({super.key});
+  MainNavigationScreen({super.key});
+
+  final screens = [
+    const Center(
+      child: Text(
+        "Home",
+      ),
+    ),
+    const Center(
+      child: Text(
+        "Search",
+      ),
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+              icon: FaIcon(
+                FontAwesomeIcons.house,
+              ),
+              label: "Home",
+              tooltip: "what are you"),
+          BottomNavigationBarItem(
+              icon: FaIcon(
+                FontAwesomeIcons.magnifyingGlass,
+              ),
+              label: "search",
+              tooltip: "what are you"),
+        ],
+      ),
+    );
   }
 }
+
+
+// assertionError: 무엇인가가 참 이기를 원하는 에러
